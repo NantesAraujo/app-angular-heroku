8296e309afdf6f67e159a67e04ca8ce4e7e76559
FAZENDA\tnaraujo <tnaraujo@fazenda.ms.gov.br>
2018-08-08 09:24:20 -0400

    adiconando o projeto

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./dist/app-angular'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname, '/dist/app-heroku/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/app-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
